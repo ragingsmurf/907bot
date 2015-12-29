5304b971d6cd12e81b473f47c12991e1d723c73a
James Davis <ragingsmurf@gmail.com>
2015-12-28 22:15:21 -0800

    Refactored SMS text copy into JSON

--- modules/cmd.services.js
+++ modules/cmd.services.js
@@ -1,9 +1,16 @@
 'use strict';
 // jscs:disable requireCapitalizedComments
 
+// Third party libraries.
+require('linq-es6');
+
 let cmd = require('./cmd')();
 let oe = require('./../modules/oe')();
 let exp = /^[0-9]{3}(\s)?[0-9]{0,2}(\s)?[0-9]{0,2}$/;
+let copy = require('./../data/copy.sms')
+  .services
+  .asEnumerable();
+
 /*
 Show: Return nodes based on nested id query.
 */
@@ -11,50 +18,49 @@ function show(query) {
   let p = new Promise(function(resolve, reject) {
     if (exp.test(query.value)) {
       let arr = query.value.split(' ');
-      let rslt = undefined;
+
       if (arr.length === 1) {
-        rslt = oe.find(arr[0]); // One Parameter
+        let rslt = oe.find(arr[0]); // One Parameter
         if (rslt.length) {
           resolve(rslt);
         } else {
-          let msg = `There doesn't appear to be any Social Services listed`;
-          msg += ` for 'Show ${arr[0]}'!\n\n`;
-          msg += `'Show All' - Get a complete list of Social Services.\n`;
-          reject(msg);
+          reject(copy
+            .single(x => x.name == 'show100')
+            .copy
+            .replace('{0}', `${arr[0]}`));
         }
       }
       if (arr.length === 2) {
-        rslt = oe.find(arr[0], `${arr[0]}-${arr[1]}`);
+        let rslt = oe.find(arr[0], `${arr[0]}-${arr[1]}`);
         if (rslt.length === 0) {
-          let msg = `There doesn't appear to be any Social Service`;
-          msg += ` sub-categories for 'Show ${arr[0]} ${arr[1]}'\n\n`;
-          msg += `'Select ${arr[0]} ${arr[1]}' - To start managing`
-          msg += ` this resource type.\n\n`;
-          msg += `'Show ${arr[0]}' - Get a complete list of Social Services`;
-          msg += ` for this category.\n`;
-          reject(msg);
+          reject(copy
+            .single(x => x.name == 'emptysub')
+            .copy
+            .replace('{0}', `${arr[0]}`)
+            .replace('{1}', `${arr[1]}`)
+            .replace('{2}', ''));
         } else {
           resolve(rslt);
         }
       }
       if (arr.length === 3) {
-        let msg = `There doesn't appear to be any Social Service`;
-        msg += ` sub-categories for 'Show ${arr[0]} ${arr[1]} ${arr[2]}'\n\n`;
-        msg += `'Select ${arr[0]} ${arr[1]}' - To start managing`;
-        msg += ` this resource type.\n\n`;
-        msg += `'Show ${arr[0]} ${arr[1]}' - To get a complete list of Social`;
-        msg += ` Services for this category.\n`;
-        reject(msg);
+        reject(copy
+          .single(x => x.name == 'emptysub')
+          .copy
+          .replace('{0}', `${arr[0]}`)
+          .replace('{1}', `${arr[1]}`)
+          .replace('{2}', `${arr[2]}`));
       }
     }
+    // Show root Open Eligibility directory
     if (query.value === 'all') {
       resolve(oe.directory());
     } else {
-      let msg = `No Social Services IDs were found for`
-      msg += ` 'Show ${query.value}'!\n\n`;
-      msg += ` 'Show All' - Get a complete list of Social Services.\n`;
-      msg += ` 'Show [number]' - Get a sub-categorized list of Services.`;
-      reject(msg);
+      // Parameter didn't match RegExp format.
+      reject(copy
+        .single(x => x.name == 'nothingmatched')
+        .copy
+        .replace('{0}', `${query.value}`));
     }
   });
   return p;
@@ -62,35 +68,42 @@ function show(query) {
 
 function select(query) {
   let p = new Promise(function(resolve, reject) {
+
     if (exp.test(query.value)) {
       let arr = query.value.split(' ');
-      let rslt = undefined;
+
+      // individual parameters.
       switch (arr.length) {
         case 1: {
           let one = arr[0];
-          reject(`Sorry, top level Social Services are not Select-able.`);
+          reject(copy
+            .single(x => x.name == 'rootunselectable')
+            .copy);
           break;
         }
         case 2: {
           let one = arr[0];
           let two = `${arr[0]}-${arr[1]}`;
           let three = undefined;
-          rslt = oe.select(one, two, three);
-          resolve(rslt);
+          resolve(oe.select(one, two, three));
           break;
         }
         case 3: {
           let one = arr[0];
           let two = `${arr[0]}-${arr[1]}`;
           let three = `${arr[0]}-${arr[1]}-${arr[2]}`;
-          rslt = oe.select(one, two, three);
-          resolve(rslt);
+          resolve(oe.select(one, two, three));
           break;
         }
         default: {
           break;
         }
       }
+    } else {
+      // Failed RegExp
+      reject(copy
+        .single(x => x.name == 'notanumber')
+        .copy);
     }
   });
   return p;
